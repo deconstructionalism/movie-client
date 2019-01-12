028cf3d920b6d2543786ababaae749b7396f6225
deconstructionalism <deconstructionalism@gmail.com>
2019-01-11 20:00:06 -0500

    Removed all unncessary routes.
    Now we only have Home, MovieIndex (for seeing and filtering all movies),
    and Add. From MovieIndex, one will be able to filter movies by criteria,
    and edit and delete any Movie.

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,6 @@
 import Home from '../views/Home.js'
 import MovieIndex from '../views/MovieIndex.js'
-import MovieShow from '../views/MovieShow.js'
 import MovieCreate from '../views/MovieCreate.js'
-import MovieUpdate from '../views/MovieUpdate.js'
-import MovieDelete from '../views/MovieDelete.js'
 
 const routes = [{
         view: Home,
@@ -13,28 +10,13 @@ const routes = [{
     {
         view: MovieIndex,
         path: '/movies',
-        linkText: 'Show All'
-    },
-    {
-        view: MovieShow,
-        path: '/movie',
-        linkText: 'Show One'
+        linkText: 'Browse'
     },
     {
         view: MovieCreate,
         path: '/add-movie',
         linkText: 'Add'
-    },
-    {
-        view: MovieUpdate,
-        path: '/update-movie',
-        linkText: 'Update'
-    },
-    {
-        view: MovieDelete,
-        path: '/delete-movie',
-        linkText: 'Delete'
-    },
+    }
 ]
 
 export default routes
