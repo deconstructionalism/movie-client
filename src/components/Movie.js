e53ddca2ea97671718e4d4fd468a343bc71f71b2
deconstructionalism <deconstructionalism@gmail.com>
2019-01-11 20:02:51 -0500

    Converted Movie to a stateful component
    It will need to keep track of whether it is being edited or not.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import '../styles/Movie.css'
 
-const Movie = props => {
-  return (
-    <div className="Movie">
-      <h1>{ props.data.title }</h1>
+class Movie extends Component {
+  render() {
+    return (
+      <div className="Movie">
+      <h1>{ this.props.data.title }</h1>
       <div className="details">
-        <p><b>ID</b>: { props.data.id }</p>
-        <p><b>Director</b>: { props.data.director }</p>
-        <p><b>Year</b>: { props.data.year }</p>
+        <p><b>ID</b>: { this.props.data.id }</p>
+        <p><b>Director</b>: { this.props.data.director }</p>
+        <p><b>Year</b>: { this.props.data.year }</p>
       </div>
     </div>
-  )
+    )
+  }
 }
 
 export default Movie
